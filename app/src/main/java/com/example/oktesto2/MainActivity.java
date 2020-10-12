dc08ec78f1c65f2ab15b31a3627e156aecbbd7b1
dstua <Dylan.stuart@ttu.edu>
2020-10-11 21:49:40 -0500

    test

---
+++ app/src/main/java/com/example/oktesto2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.oktesto2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button Logout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Logout = findViewById(R.id.Logout);
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent _login = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(_login);
+            }
+        });
+    }
+}
