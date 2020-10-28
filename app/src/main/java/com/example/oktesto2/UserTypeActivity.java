e26f28dbab5f92ad8bff329bf695b889dc7e1e63
Barka-4966 <sydney.pearson@ttu.edu>
2020-10-28 11:24:01 -0500

    UI changes.

---
+++ app/src/main/java/com/example/oktesto2/UserTypeActivity.java
@@ -0,0 +1,37 @@
+package com.example.oktesto2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class UserTypeActivity extends AppCompatActivity {
+    ImageView Adopter, Lister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type);
+        Adopter = findViewById(R.id.btn_adopter);
+        Lister = findViewById(R.id.btn_lister);
+
+        Adopter = findViewById(R.id.btn_adopter);
+        Adopter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activityChangeIntent = new Intent(UserTypeActivity.this, RegisterActivity.class);
+                startActivity(activityChangeIntent);
+            }
+        });
+
+        Lister = findViewById(R.id.btn_lister);
+        Lister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activityChangeIntent = new Intent(UserTypeActivity.this, RegisterActivity.class);
+                startActivity(activityChangeIntent);
+            }
+        });
+    }
+}
